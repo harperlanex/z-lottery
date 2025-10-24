03642cb573133977cc8193066e25affebda1e1fc
harperlanex <ebjfghgn832@outlook.com>
2025-10-24 19:05:27 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedLottery = await deploy("ZamaLottery", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ZamaLottery contract: `, deployedLottery.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zamaLottery"; // id required to prevent reexecution
+func.tags = ["ZamaLottery"];
